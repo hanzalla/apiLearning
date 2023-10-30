4fb8e6d5fedcbb9f14d0e43e3df7c1dc6c5c396e
hanzalla-workstreamautomation <muzaffar.hanzalla@workstreamautomation.com>
2023-10-30 13:19:49 +0500

    card formation done learning ap-Javascript

--- app.js
+++ app.js
@@ -3,11 +3,32 @@ const xhr = new XMLHttpRequest();
 xhr.open('GET', requestUrl);
 xhr.onreadystatechange = function () {
     console.log(xhr.readyState);
+    let container = document.querySelector('.container');
+    let image = document.querySelector('img');
+   
+    const userName = document.createElement('p');
+    const followers = document.createElement('p');
+    console.log(image);
+    console.log(container);
     if (xhr.readyState === 4) {
         const data = JSON.parse(this.responseText);
+        image.src = data.avatar_url;
+        userName.innerHTML = `<h2>${data.name}</h2>`;
+        let general = null;
+         for (let index = 0; index < 3; index++) {
+             general = document.createElement('p');
+            //  general.setAttribute('value', index);
+            //  console.log(general);
+             container.appendChild(general);
+            }
+            general.innerHTML = `<br><h1>User Name: ${data.name}</h1><b>followers:${data.followers}</b><br><br><b>followings:${data.following}</b><br>`;
+        // container.appendChild(userName);
         console.log(typeof data);
-        console.log(data.followers)
+        console.log(`followers is/are ${data.followers}`);
     }
     
 }
 xhr.send();
+
+
+
