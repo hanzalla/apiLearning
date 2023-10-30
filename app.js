dd51657749df00fff6b313a4aff23f60dc4b6ddd
hanzalla-workstreamautomation <muzaffar.hanzalla@workstreamautomation.com>
2023-10-30 14:58:19 +0500

    card added with custom image

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Your JavaScript code here
 const requestUrl = `https://api.github.com/users/hanzalla`;
 const xhr = new XMLHttpRequest();
 xhr.open('GET', requestUrl);
@@ -8,27 +10,24 @@ xhr.onreadystatechange = function () {
    
     const userName = document.createElement('p');
     const followers = document.createElement('p');
+    const following = document.createElement('p');
     console.log(image);
     console.log(container);
     if (xhr.readyState === 4) {
         const data = JSON.parse(this.responseText);
         image.src = data.avatar_url;
-        userName.innerHTML = `<h2>${data.name}</h2>`;
-        let general = null;
-         for (let index = 0; index < 3; index++) {
-             general = document.createElement('p');
-            //  general.setAttribute('value', index);
-            //  console.log(general);
-             container.appendChild(general);
-            }
-            general.innerHTML = `<br><h1>User Name: ${data.name}</h1><b>followers:${data.followers}</b><br><br><b>followings:${data.following}</b><br>`;
-        // container.appendChild(userName);
+        userName.innerHTML = `<h2>User Name: ${data.name}</h2>`;
+        followers.innerHTML = `<h2>Followers:<span>${data.followers}</span></h2>`
+        following.innerHTML = `<h2>Following:<span>${data.following}</span></h2>`
         console.log(typeof data);
         console.log(`followers is/are ${data.followers}`);
     }
+    container.appendChild(userName);
+    container.appendChild(followers);
+    container.appendChild(following);
     
 }
 xhr.send();
 
 
-
+});
